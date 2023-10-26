9315ce3be76f371cb2136a11875e941759ad3a69
yojanz22 <yoj.cortes@duocuc.cl>
2023-10-26 13:51:19 -0300

    geo

--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,26 +9,119 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  final Set<Marker> _markers = {};
+  late Position _currentPosition;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  void _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Si los servicios de ubicación no están habilitados, muestra un mensaje.
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Activar Ubicación'),
+            content: Text(
+                'Por favor, active los servicios de ubicación para usar esta aplicación.'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Si se deniegan los permisos, muestra un mensaje.
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Permiso de Ubicación Denegado'),
+              content: Text(
+                  'Debes otorgar permisos de ubicación para usar esta aplicación.'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      _currentPosition = position;
+    });
+  }
+
+  void _goToCurrentLocation() {
+    if (_currentPosition != null && mapController != null) {
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target:
+                LatLng(_currentPosition.latitude, _currentPosition.longitude),
+            zoom: 14.0,
+          ),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Maps Sample App'),
+        title: Text('Map Sample App'),
         backgroundColor: Colors.green[700],
       ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
+          target: LatLng(
+            _currentPosition?.latitude ?? 0.0,
+            _currentPosition?.longitude ?? 0.0,
+          ),
+          zoom: 14.0,
         ),
+        markers: _markers,
+        myLocationEnabled: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToCurrentLocation,
+        child: Icon(Icons.location_on),
       ),
     );
   }
