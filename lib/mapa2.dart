abe177a826793b6fddd802572521d6442ca823b6
Yojann Brandon Cortes Diaz <102491394+yojanz22@users.noreply.github.com>
2023-10-30 21:01:34 -0300

    multiple vistas
    se agrega ver muchas vistas

--- lib/mapa2.dart
+++ lib/mapa2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Mapa2Page extends StatefulWidget {
   final Position location;
@@ -14,6 +15,39 @@ class Mapa2Page extends StatefulWidget {
 class _Mapa2PageState extends State<Mapa2Page> {
   GoogleMapController? _controller;
   Set<Marker> _markers = Set<Marker>();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMarkersFromFirestore();
+  }
+
+  void _loadMarkersFromFirestore() {
+    FirebaseFirestore.instance
+        .collection('producto')
+        .get()
+        .then((querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        final GeoPoint ubicacion = data['ubicacion'] as GeoPoint;
+        final String nombre = data['nombre'] as String;
+
+        _markers.add(
+          Marker(
+            markerId: MarkerId(doc.id),
+            position: LatLng(ubicacion.latitude, ubicacion.longitude),
+            infoWindow: InfoWindow(
+              title: nombre,
+            ),
+          ),
+        );
+
+        setState(() {
+          _markers = _markers;
+        });
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +64,6 @@ class _Mapa2PageState extends State<Mapa2Page> {
         onMapCreated: (GoogleMapController controller) {
           setState(() {
             _controller = controller;
-            _markers.add(
-              Marker(
-                markerId: MarkerId('ubicacion'),
-                position:
-                    LatLng(widget.location.latitude, widget.location.longitude),
-              ),
-            );
           });
         },
       ),
