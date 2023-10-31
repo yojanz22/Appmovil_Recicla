513006c0eaeece45fc9a15bd65717a53acf2d4a8
Yojann Brandon Cortes Diaz <102491394+yojanz22@users.noreply.github.com>
2023-10-30 21:17:45 -0300

    ver dato

--- lib/mapa2.dart
+++ lib/mapa2.dart
@@ -22,6 +22,35 @@ class _Mapa2PageState extends State<Mapa2Page> {
     _loadMarkersFromFirestore();
   }
 
+  void _showProductInfo(String nombre, String descripcion, String direccion,
+      String tipoDeMaterial) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(nombre), // Muestra el nombre del producto como título
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Descripción: $descripcion"),
+              Text("Dirección: $direccion"),
+              Text("Tipo de Material: $tipoDeMaterial"),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Cierra el cuadro de diálogo
+              },
+              child: Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void _loadMarkersFromFirestore() {
     FirebaseFirestore.instance
         .collection('producto')
@@ -31,16 +60,20 @@ class _Mapa2PageState extends State<Mapa2Page> {
         final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
         final GeoPoint ubicacion = data['ubicacion'] as GeoPoint;
         final String nombre = data['nombre'] as String;
+        final String descripcion = data['descripcion'] as String;
+        final String direccion = data['direccion'] as String;
+        final String tipoDeMaterial = data['tipoDeMaterial'] as String;
 
-        _markers.add(
-          Marker(
-            markerId: MarkerId(doc.id),
-            position: LatLng(ubicacion.latitude, ubicacion.longitude),
-            infoWindow: InfoWindow(
-              title: nombre, // Agrega el título del marcador
-            ),
+        _markers.add(Marker(
+          markerId: MarkerId(doc.id),
+          position: LatLng(ubicacion.latitude, ubicacion.longitude),
+          infoWindow: InfoWindow(
+            title: nombre, // Agrega el título del marcador
           ),
-        );
+          onTap: () {
+            _showProductInfo(nombre, descripcion, direccion, tipoDeMaterial);
+          },
+        ));
 
         setState(() {
           _markers = _markers;
