ec79191fac199b27298e4d7697d754b2d84ef6f2
Yojann Brandon Cortes Diaz <102491394+yojanz22@users.noreply.github.com>
2023-11-02 00:44:45 -0300

    se elimina mapa1

--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
-import 'crear_anuncio.dart'; // Importa la página de Crear Anuncio
+import 'crear_anuncio.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -10,19 +8,12 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController mapController;
-  final Set<Marker> _markers = {};
   late Position _currentPosition;
-  TextEditingController _addressController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _getCurrentLocation();
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
   }
 
   void _getCurrentLocation() async {
@@ -31,25 +22,7 @@ class _MapScreenState extends State<MapScreen> {
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Activar Ubicación'),
-            content: Text(
-              'Por favor, active los servicios de ubicación para usar esta aplicación.',
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
+      // Manejo de la falta de servicio de ubicación
       return;
     }
 
@@ -57,25 +30,7 @@ class _MapScreenState extends State<MapScreen> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text('Permiso de Ubicación Denegado'),
-              content: Text(
-                'Debes otorgar permisos de ubicación para usar esta aplicación.',
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        );
+        // Manejo de la denegación de permisos de ubicación
         return;
       }
     }
@@ -89,42 +44,6 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
-  Future<void> _searchLocation() async {
-    List<Location> locations =
-        await locationFromAddress(_addressController.text);
-    if (locations.isNotEmpty) {
-      Location location = locations[0];
-      if (mapController != null) {
-        mapController.animateCamera(
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: LatLng(location.latitude, location.longitude),
-              zoom: 14.0,
-            ),
-          ),
-        );
-      }
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Dirección no encontrada'),
-            content: Text('No se pudo encontrar la dirección ingresada.'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -132,75 +51,25 @@ class _MapScreenState extends State<MapScreen> {
         title: Text('Map Sample App'),
         backgroundColor: Colors.green[700],
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _addressController,
-                    decoration: InputDecoration(
-                      labelText: 'Buscar dirección',
-                      border: OutlineInputBorder(),
-                    ),
+      body: Center(
+        // Aquí se encuentra el botón "Crear Anuncio"
+        child: ElevatedButton(
+          onPressed: () {
+            if (_currentPosition != null) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CrearAnuncioPage(
+                    location: _currentPosition,
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: _searchLocation,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                  _currentPosition?.latitude ?? 0.0,
-                  _currentPosition?.longitude ?? 0.0,
-                ),
-                zoom: 14.0,
-              ),
-              markers: _markers,
-              myLocationEnabled: true,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CrearAnuncioPage(
-                      location: _currentPosition,
-                    ),
-                  ),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.add),
-                  Text('Crear Anuncio'),
-                ],
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.green,
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+              );
+            } else {
+              // Manejo de la falta de ubicación actual
+            }
+          },
+          child: Text('Crear Anuncio'),
+        ),
       ),
     );
   }
